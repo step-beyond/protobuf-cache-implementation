a81a63ec8d25e62f92db799b86b6b51203821ec1
Jonas Huebner <dev@jonashuebner.de>
2023-10-18 13:46:08 +0200

    feat: add some abstractions and rename classes

--- src/main/java/com/step/beyond/examples/person/infrastructure/PersonCache.java
+++ src/main/java/com/step/beyond/examples/person/infrastructure/PersonCache.java
@@ -1,29 +1,30 @@
 package com.step.beyond.examples.person.infrastructure;
 
 import com.step.beyond.examples.cache.Cache;
+import com.step.beyond.examples.cache.ProtoFileStorage;
 import com.step.beyond.examples.person.domain.model.PersonDomain;
 import com.step.beyond.examples.person.infrastructure.mapper.PersonMapper;
 import com.step.beyond.examples.person.infrastructure.repository.model.gen.Person;
 import java.util.Optional;
 
-public class PersonCacheRepository {
+public class PersonCache {
 
-    private static String KEY_PATTERN = "%s.bin";
+    private static final String KEY_PATTERN = "%s.bin";
 
-    private final Cache<Person> cache = Cache.withParser(Person.parser());
+    private final Cache<Person> cache = ProtoFileStorage.withParser(Person.parser());
     private final PersonMapper mapper;
 
-    public PersonCacheRepository(PersonMapper mapper) {
+    public PersonCache(PersonMapper mapper) {
         this.mapper = mapper;
     }
 
-    public void writePerson(PersonDomain person) {
+    public boolean putPerson(PersonDomain person) {
         Person personProto = mapper.map(person);
-        cache.write(String.format(KEY_PATTERN, "Person"), personProto);
+        return cache.put(String.format(KEY_PATTERN, "Person"), personProto);
     }
 
     public Optional<PersonDomain> findPerson() {
-        return Optional.ofNullable(cache.read(String.format(KEY_PATTERN, "Person")))
+        return Optional.ofNullable(cache.get(String.format(KEY_PATTERN, "Person")))
                 .map(mapper::map);
     }
 }
